ee326b1ef7ce1f07dd0324b86375694989cdc25a
SkyBlueHeat <br.aydin@hotmail.com>
2024-10-24 14:30:13 +0300

    2. Görev Tamamlandı

--- index.js
+++ index.js
@@ -64,8 +64,8 @@ function CemberinCevresi(cevre) {
 	4. Hesaplanan çemberin alanı döndürülecektir.
 */
 
-function CemberinAlani(/* kodlar buraya */) {
-  /* kodlar buraya */
+function CemberinAlani(yariCap, pi) {
+ return pi * Math.pow(yariCap, 2);
 }
 
 /* (Oto test yok) Yukarıdaki CemberinAlani fonksiyonunu yarıçap = 15 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
